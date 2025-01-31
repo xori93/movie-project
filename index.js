c46a10b9fb3deb40cb0b91699bdb830ffc07e9f5
Xori <xori93@aol.com>
2025-01-30 22:57:56 -0500

    working on sort options

--- index.js
+++ index.js
@@ -5,6 +5,129 @@ const searchBar = document.getElementById('search');
 const searchIcon = document.querySelector('.search__icon')
 let movieList = [];
 
+
+async function main() {
+  const response = await fetch(`https://www.omdbapi.com/?apikey=46b68acf&s=marvel`);
+  const data = await response.json();
+  movieList = data.Search;
+  
+  const movieListElement = document.querySelector(".movie-list");
+  movieListElement.innerHTML = movieList.map(
+    (movie) => 
+      renderMovies(movie)
+  ).join("");
+}
+
+searchBar.addEventListener('keyup', async (e) => {
+  const search = e.target.value.trim();
+  if (search.length >= 3) {
+    const response = await fetch(`http://www.omdbapi.com/?apikey=46b68acf&s=${search}`);
+    const data = await response.json();
+    movieList = data.Search || [];
+
+    const movieListElement = document.querySelector(".movie-list");
+    movieListElement.innerHTML = movieList.map(
+      (movie) => 
+        renderMovies(movie)
+    ).join("");
+  }
+});
+
+searchIcon.addEventListener("click", () => {
+  fetchMovies(searchBar.value.trim());
+});
+
+
+function filterBooks(event) {
+  if (event.target.value === 'LATEST_MOVIE') {
+    console.log('sort latest mvovie')
+    
+  }
+}
+
+/*
+fetchMovie.addEventListener("click", async(e) => {
+  const searchIcon = e.target.value.trim();
+  if (searchIcon.length >= 3) {
+    const response = await fetch(`http://www.omdbapi.com/?apikey=46b68acf&s=${search}`);
+    const data = await response.json();
+    movieList = data.Search || [];
+  
+    const movieListElement = document.querySelector(".movie-list");
+    movieListElement.innerHTML = movieList.map(
+      (movie) => 
+        renderMovies(movie)
+    ).join("");
+  }
+})
+
+*/
+
+main();
+
+function renderMovies(movie) {
+  return `<div class="container">
+        <div class="row">
+          <div class="movie-list">
+            <div class="movie__wrapper">
+              <div class= "movie__container">
+              <img src="${movie.Poster}" class="movie__img"/>
+              </div>
+              <div class="movie__wrapper--bg"></div>
+              <div class="movie-info">
+                <h3>${movie.Title}</h3>
+                <p class="movie__type">${movie.Type}</p>
+                <p class="movie__year">${movie.Year}</p>
+            </div>
+            </div>
+          </div>
+        </div>
+    </div>`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+const searchBar = document.getElementById('search');
+const searchIcon = document.querySelector('.search__icon')
+let movieList = [];
+
 async function fetchMovies(query) {
   if (query.length < 3) return; 
   
@@ -64,3 +187,4 @@ function renderMovies(movie) {
     </div>`
 }
 
+*/
