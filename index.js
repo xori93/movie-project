9693b61909b3a9e724a64313933a9debdf9d9b78
Xori <xori93@aol.com>
2025-02-02 15:37:54 -0500

    search bar done

--- index.js
+++ index.js
@@ -2,16 +2,18 @@
 // http://www.omdbapi.com/?i=tt3896198&apikey=46b68acf
 
 const searchBar = document.getElementById('search');
-const searchIcon = document.querySelector('.search__icon');
+const searchMovie = document.querySelector('.search__icon');
 const filterSelect = document.getElementById("filter");
+const searchForm = document.getElementById('form')
 let movieList = [];
 
 
-async function main(filter) {
 
-  if (filter === 'LATEST_MOVIE') {
-    console.log(filter)
-  }
+
+async function main(filter) {
+    if (filter === 'LATEST_MOVIE') {
+        console.log(filter)
+      }
 
   const response = await fetch(`https://www.omdbapi.com/?apikey=46b68acf&s=marvel`);
   const data = await response.json();
@@ -24,31 +26,50 @@ async function main(filter) {
   ).join("");
 }
 
-searchBar.addEventListener('keyup', async (e) => {
-  const search = e.target.value.trim();
-  if (search.length >= 3) {
-    const response = await fetch(`http://www.omdbapi.com/?apikey=46b68acf&s=${search}`);
-    const data = await response.json();
-    movieList = data.Search || [];
 
-    const movieListElement = document.querySelector(".movie-list");
-    movieListElement.innerHTML = movieList.map(
-      (movie) => 
-        renderMovies(movie)
-    ).join("");
-  }
-});
-
-searchIcon.addEventListener("click", () => {
-  fetchMovies(searchBar.value.trim());
-});
-
-
-function filterMovie(event) {
-
-    renderMovies(event.target.value)
+async function search() {
+  const searchText = searchBar.value;
+  const response = await fetch(`https://www.omdbapi.com/?apikey=46b68acf&s=${searchText}`);
+  const data = await response.json();
+  movieList = data.Search;
   
+  const movieListElement = document.querySelector(".movie-list");
+  movieListElement.innerHTML = movieList.map(
+    (movie) => 
+      renderMovies(movie)
+  ).join("");
 }
+
+searchForm.addEventListener('submit', async (event) => {
+  event.preventDefault()
+ await search()
+})
+
+searchMovie.addEventListener('click', async() => {
+ await search()
+});
+
+// searchBar.addEventListener('keyup', async (e) => {
+//   const search = e.target.value.trim();
+//   if (search.length >= 3) {
+//     const response = await fetch(`http://www.omdbapi.com/?apikey=46b68acf&s=${search}`);
+//     const data = await response.json();
+//     movieList = data.Search || [];
+
+//     const movieListElement = document.querySelector(".movie-list");
+//     movieListElement.innerHTML = movieList.map(
+//       (movie) => 
+//         renderMovies(movie)
+//     ).join("");
+//   }
+
+
+
+// function filterMovie(event) {
+
+//     renderMovies(event.target.value)
+  
+// }
 
 /*
 fetchMovie.addEventListener("click", async(e) => {
@@ -89,107 +110,3 @@ function renderMovies(movie) {
         </div>
     </div>`
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-const searchBar = document.getElementById('search');
-const searchIcon = document.querySelector('.search__icon')
-let movieList = [];
-
-async function fetchMovies(query) {
-  if (query.length < 3) return; 
-  
-  try {
-    const response = await fetch(`https://www.omdbapi.com/?apikey=46b68acf&s=${query}`);
-    const data = await response.json();
-    movieList = data.Search || [];
-
-    const movieListElement = document.querySelector(".movie-list");
-    movieListElement.innerHTML = movieList.map((movie) => renderMovies(movie)).join("");
-  } catch (error) {
-    console.error("Error fetching movie data:", error);
-  }
-}
-
-
-searchBar.addEventListener("keyup", async (e) => {
-  fetchMovies(e.target.value.trim());
-});
-
-searchIcon.addEventListener("click", () => {
-  fetchMovies(searchBar.value.trim());
-});
-
-
-async function main() {
-  const response = await fetch(`https://www.omdbapi.com/?apikey=46b68acf&s=marvel`);
-  const data = await response.json();
-  movieList = data.Search;
-  
-  const movieListElement = document.querySelector(".movie-list");
-  movieListElement.innerHTML = movieList.map(
-    (movie) => 
-      renderMovies(movie)
-  ).join("");
-}
-
-main();
-
-function renderMovies(movie) {
-  return `<div class="container">
-        <div class="row">
-          <div class="movie-list">
-            <div class="movie__wrapper">
-              <div class= "movie__container">
-              <img src="${movie.Poster}" class="movie__img"/>
-              </div>
-              <div class="movie__wrapper--bg"></div>
-              <div class="movie-info">
-                <h3>${movie.Title}</h3>
-                <p class="movie__type">${movie.Type}</p>
-                <p class="movie__year">${movie.Year}</p>
-            </div>
-            </div>
-          </div>
-        </div>
-    </div>`
-}
-
-*/
