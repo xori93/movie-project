6117bfa12a7d67e0980086f80654d8a76357c655
Xori <xori93@aol.com>
2025-02-03 20:52:21 -0500

    final project

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-// api key = 46b68acf
-// http://www.omdbapi.com/?i=tt3896198&apikey=46b68acf
-
 const searchBar = document.getElementById('search');
 const searchMovie = document.querySelector('.search__icon');
 const filterDropdown = document.getElementById("filter");
 const searchForm = document.getElementById('form')
 let movieList = [];
 
-
-
-
-async function main(filter) {
-  if (filterOption === "LATEST_MOVIE") {
-    console.log(filter)
-    // console.log(filterOption)
-  }
+async function main() {
 
   const response = await fetch(`https://www.omdbapi.com/?apikey=46b68acf&s=marvel`);
   const data = await response.json();
@@ -28,10 +18,15 @@ async function main(filter) {
 }
 
 
+// The search function will be async
 async function search() {
+  // store the value of the input
   const searchText = searchBar.value;
+  // make an api call. and show the value of searchText
   const response = await fetch(`https://www.omdbapi.com/?apikey=46b68acf&s=${searchText}`);
+  // make the api frontend
   const data = await response.json();
+  // store the movielist in data
   movieList = data.Search;
   
   const movieListElement = document.querySelector(".movie-list");
@@ -41,58 +36,37 @@ async function search() {
   ).join("");
 }
 
+// add event listener to the form 
 searchForm.addEventListener('submit', async (event) => {
+  // prevent the page from auto re-load
   event.preventDefault()
+  // call the search funtion to show the movies
  await search()
 })
 
+// when user click the search, show the searched movie
 searchMovie.addEventListener('click', async() => {
  await search()
 });
 
-// searchBar.addEventListener('keyup', async (e) => {
-//   const search = e.target.value.trim();
-//   if (search.length >= 3) {
-//     const response = await fetch(`http://www.omdbapi.com/?apikey=46b68acf&s=${search}`);
-//     const data = await response.json();
-//     movieList = data.Search || [];
-
-//     const movieListElement = document.querySelector(".movie-list");
-//     movieListElement.innerHTML = movieList.map(
-//       (movie) => 
-//         renderMovies(movie)
-//     ).join("");
-//   }
-
-
 
 function filterMovie(event) {
 
-  const filterOption = event.target.value
+  const filterOption = event.target.value;
+  let filteredMovies = [...movieList];
   
-  renderMovies(event.target.value)
-
-    // renderMovies(event.target.value)
-  
+  if (filterOption === "LATEST_MOVIE") {
+    filteredMovies.sort((a, b) => parseInt(b.Year) - parseInt(a.Year));
+  } else if (filterOption === "OLDEST_MOVIE") {
+    filteredMovies.sort((a, b) => parseInt(a.Year) - parseInt(b.Year));
+  }
+  const movieListElement = document.querySelector(".movie-list");
+  movieListElement.innerHTML = filteredMovies.map(
+    (movie) => renderMovies(movie)
+  ).join("");  
 }
 
-/*
-fetchMovie.addEventListener("click", async(e) => {
-  const searchIcon = e.target.value.trim();
-  if (searchIcon.length >= 3) {
-    const response = await fetch(`http://www.omdbapi.com/?apikey=46b68acf&s=${search}`);
-    const data = await response.json();
-    movieList = data.Search || [];
-  
-    const movieListElement = document.querySelector(".movie-list");
-    movieListElement.innerHTML = movieList.map(
-      (movie) => 
-        renderMovies(movie)
-    ).join("");
-  }
-})
-
-*/
+filterDropdown.addEventListener('change', filterMovie);
 
 main();
 
